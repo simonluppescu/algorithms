037479355e771c237eb5c5a7ef9eb4fcbd88ad86
Simon Luppescu <simon.luppescu@gmail.com>
2019-09-08 18:32:50 +0900

    Spider Jumping to Fly on web for crowdcast - first version

---
+++ spiderJumpingToFlyOnWeb.js
@@ -0,0 +1,53 @@
+/*
+A spider web is defined by
+
+"rings" numbered out from the centre as 0, 1, 2, 3, 4
+"radials" labelled clock-wise from the top as A, B, C, D, E, F, G, H
+
+For example the spider is at H3 and the fly is at E2.
+
+Your task is to calculate and return the distance the spider must jump to get to the fly.
+*/
+
+class JumpDistanceCalculator {
+  constructor(spiderCoord, flyCoord) {
+    this.spiderCoord = spiderCoord;
+    this.flyCoord = flyCoord;
+  }
+
+  calculate() {
+    let radialDifference = Math.abs(
+      this.spiderCoord.radial - this.flyCoord.radial
+    );
+    radialDifference = Math.min(radialDifference, 8 - radialDifference);
+    const radialDifferenceRadians = (radialDifference / 8) * 2 * Math.PI;
+
+    const spiderRing = this.spiderCoord.ring;
+    const flyRing = this.flyCoord.ring;
+    const distance = Math.sqrt(
+      spiderRing * spiderRing +
+        flyRing * flyRing -
+        2 * spiderRing * flyRing * Math.cos(radialDifferenceRadians)
+    );
+
+    return distance;
+  }
+}
+
+class Coordinate {
+  constructor(coordStr) {
+    if (coordStr.length !== 2) {
+      throw new Error("coordinate must be length 2.");
+    }
+
+    const radialMapper = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7 };
+
+    this.radial = radialMapper[coordStr[0]];
+    this.ring = parseInt(coordStr[1]);
+  }
+}
+
+const spider = new Coordinate("H2");
+const fly = new Coordinate("H2");
+const calculator = new JumpDistanceCalculator(spider, fly);
+console.log(calculator.calculate());
