733e7eda488a1eb24edee8515ef585d8c0972df9
Simon Luppescu <simon.luppescu@gmail.com>
2019-10-19 16:14:25 +0900

    Added DFS iterative version

---
+++ depthFirstSearchIterative.ts
@@ -0,0 +1,39 @@
+import BinaryTree, { BinaryTreeNode } from "./utils/binaryTree";
+import Stack from "./utils/stack";
+
+class IterativeDFS {
+  tree: BinaryTree;
+
+  constructor(bt: BinaryTree) {
+    this.tree = bt;
+  }
+
+  print(): void {
+    const nodes = new Stack<BinaryTreeNode>();
+
+    let currNode = this.tree.root;
+    currNode.visit();
+    nodes.push(currNode);
+
+    while (!nodes.isEmpty()) {
+      if (currNode.hasLeft()) {
+        currNode = currNode.left;
+        currNode.visit();
+        nodes.push(currNode);
+      } else {
+        currNode = nodes.pop();
+        console.log(currNode.value);
+
+        if (!currNode.hasLeft() && currNode.hasRight()) {
+          currNode = currNode.right;
+          currNode.visit();
+          nodes.push(currNode);
+        }
+      }
+    }
+  }
+}
+
+const bt = BinaryTree.randomBST();
+const foo = new IterativeDFS(bt);
+foo.print();
---
+++ js_output/depthFirstSearchIterative.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var binaryTree_1 = require("./utils/binaryTree");
+var stack_1 = require("./utils/stack");
+var IterativeDFS = /** @class */ (function () {
+    function IterativeDFS(bt) {
+        this.tree = bt;
+    }
+    IterativeDFS.prototype.print = function () {
+        var nodes = new stack_1.default();
+        var currNode = this.tree.root;
+        currNode.visit();
+        nodes.push(currNode);
+        while (!nodes.isEmpty()) {
+            if (currNode.hasLeft()) {
+                currNode = currNode.left;
+                currNode.visit();
+                nodes.push(currNode);
+            }
+            else {
+                currNode = nodes.pop();
+                console.log(currNode.value);
+                if (!currNode.hasLeft() && currNode.hasRight()) {
+                    currNode = currNode.right;
+                    currNode.visit();
+                    nodes.push(currNode);
+                }
+            }
+        }
+    };
+    return IterativeDFS;
+}());
+var bt = binaryTree_1.default.randomBST();
+var foo = new IterativeDFS(bt);
+foo.print();
