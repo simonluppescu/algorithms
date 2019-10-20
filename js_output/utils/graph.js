43396e4f794beb3e1f3f08c2f26bdf3cc173d98d
Simon Luppescu <simon.luppescu@gmail.com>
2019-10-21 01:44:03 +0900

    Added edges to graph

--- js_output/utils/graph.js
+++ js_output/utils/graph.js
@@ -1,8 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var assert = require("assert");
 var Graph = /** @class */ (function () {
     function Graph(root) {
         this.edges = {};
         this.root = root;
     }
+    Graph.prototype.addAdjacent = function (origin, newAdjacent, edge) {
+        if (edge === void 0) { edge = null; }
+        var dest = null;
+        if (newAdjacent instanceof GraphNode) {
+            dest = newAdjacent;
+        }
+        else {
+            dest = new GraphNode(newAdjacent);
+        }
+        origin.adjacents.push(dest);
+        dest.adjacents.push(origin);
+        if (!edge) {
+            this.edges[origin.value][dest.value] = edge;
+        }
+    };
+    Graph.prototype.includes = function (value) {
+        var includesHelper = function (node) {
+            if (node.value === value)
+                return true;
+            node.visit();
+            var result = false;
+            node.adjacents.forEach(function (adjacent) {
+                if (!adjacent.visited) {
+                    result = result || includesHelper(adjacent);
+                }
+            });
+            return result;
+        };
+        return includesHelper(this.root);
+    };
+    Graph.generateForDijkstra = function () {
+        var tokyo = new GraphNode("Tokyo");
+        var seoul = new GraphNode("Seoul");
+        var bangkok = new GraphNode("Bangkok");
+        var moscow = new GraphNode("Moscow");
+        var nepal = new GraphNode("Nepal");
+        var g = new Graph(tokyo);
+        g.addAdjacent(tokyo, seoul, 10);
+        g.addAdjacent(tokyo, bangkok, 15);
+        g.addAdjacent(bangkok, seoul, 20);
+        g.addAdjacent(seoul, moscow, 30);
+        g.addAdjacent(nepal, moscow, 25);
+        g.addAdjacent(nepal, bangkok, 20);
+        return g;
+    };
     return Graph;
 }());
 var GraphNode = /** @class */ (function () {
@@ -11,16 +59,12 @@ var GraphNode = /** @class */ (function () {
         this.adjacents = [];
         this.visited = false;
     }
-    GraphNode.prototype.addAdjacent = function (newAdjacent) {
-        if (typeof newAdjacent === "number") {
-            this.adjacents.push(new GraphNode(newAdjacent));
-        }
-        else if (newAdjacent instanceof GraphNode) {
-            this.adjacents.push(newAdjacent);
-        }
-    };
-    GraphNode.generateForDijkstra = function () {
-        // hi
+    GraphNode.prototype.visit = function () {
+        this.visited = true;
     };
     return GraphNode;
 }());
+var g = Graph.generateForDijkstra();
+assert(g.includes("Tokyo") === true);
+assert(g.includes("Hi") === false);
+console.log("Assertions passed for includes()");
--- utils/graph.ts
+++ utils/graph.ts
@@ -1,3 +1,5 @@
+import * as assert from "assert";
+
 class Graph {
   edges: { [val: number]: { [val2: number]: number } };
   root: GraphNode;
@@ -6,28 +8,78 @@ class Graph {
     this.edges = {};
     this.root = root;
   }
+
+  addAdjacent(origin: GraphNode, newAdjacent: string | GraphNode, edge: number = null): void {
+    let dest = null;
+    if (newAdjacent instanceof GraphNode) {
+      dest = newAdjacent;
+    } else {
+      dest = new GraphNode(newAdjacent);
+    }
+    origin.adjacents.push(dest);
+    dest.adjacents.push(origin);
+
+    if (!edge) {
+      this.edges[origin.value][dest.value] = edge;
+    }
+  }
+
+  includes(value: string): boolean {
+    const includesHelper = (node: GraphNode): boolean => {
+      if (node.value === value) return true;
+
+      node.visit();
+
+      let result = false;
+      node.adjacents.forEach((adjacent) => {
+        if (!adjacent.visited) {
+          result = result || includesHelper(adjacent);
+        }
+      });
+
+      return result;
+    };
+
+    return includesHelper(this.root);
+  }
+
+  static generateForDijkstra(): Graph {
+    const tokyo = new GraphNode("Tokyo");
+    const seoul = new GraphNode("Seoul");
+    const bangkok = new GraphNode("Bangkok");
+    const moscow = new GraphNode("Moscow");
+    const nepal = new GraphNode("Nepal");
+
+    const g = new Graph(tokyo);
+    g.addAdjacent(tokyo, seoul, 10);
+    g.addAdjacent(tokyo, bangkok, 15);
+    g.addAdjacent(bangkok, seoul, 20);
+    g.addAdjacent(seoul, moscow, 30);
+    g.addAdjacent(nepal, moscow, 25);
+    g.addAdjacent(nepal, bangkok, 20);
+
+    return g;
+  }
 }
 
 class GraphNode {
   adjacents: GraphNode[];
   visited: boolean;
-  value: number;
+  value: string;
 
-  constructor(value: number) {
+  constructor(value: string) {
     this.value = value;
     this.adjacents = [];
     this.visited = false;
   }
 
-  addAdjacent(newAdjacent: number | GraphNode): void {
-    if (typeof newAdjacent === "number") {
-      this.adjacents.push(new GraphNode(newAdjacent));
-    } else if (newAdjacent instanceof GraphNode) {
-      this.adjacents.push(newAdjacent);
-    }
-  }
-
-  static generateForDijkstra(): GraphNode {
-    // hi
+  visit(): void {
+    this.visited = true;
   }
 }
+
+const g = Graph.generateForDijkstra();
+
+assert(g.includes("Tokyo") === true);
+assert(g.includes("Hi") === false);
+console.log("Assertions passed for includes()");
