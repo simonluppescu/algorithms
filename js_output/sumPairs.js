fed29a2129f5480ef4e022ad4ecf56e0a78a689a
Simon Luppescu <simon.luppescu@gmail.com>
2019-12-14 23:10:08 +0900

    Added find pair of elements in array whose sum equals k

---
+++ js_output/sumPairs.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Category: ARRAYS
+ * Tags: math maps
+ *
+ * Given an array of integers, find a pair of indices whose values sum to a value, k.
+ */
+var assertArray_1 = require("./utils/assertArray");
+function findPairs(arr, k) {
+    var found = {};
+    for (var i = 0; i < arr.length; i++) {
+        var value = arr[i];
+        if (found[value]) {
+            return [found[value], i];
+        }
+        else {
+            found[k - value] = i;
+        }
+    }
+    return [];
+}
+assertArray_1.default(findPairs([4, 6, 10, 12, 18], 18), [1, 3]);
+assertArray_1.default(findPairs([4, 6, 10, 12, 18], 16), [1, 2]);
+assertArray_1.default(findPairs([1, 3, 7, 2, 3, 10], 6), [1, 4]);
+assertArray_1.default(findPairs([1, 2, 3, 4, 5], 10), []);
+assertArray_1.default(findPairs([1, 3, 4, 2], 4), []);
+console.log("All assertions passed");
