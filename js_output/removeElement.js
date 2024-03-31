a25700fa563f36504c895b3becba102045f08d76
Simon <simon.luppescu@gmail.com>
2024-03-31 21:37:54 +0900

    Added easy 'remove element' problem

---
+++ js_output/removeElement.js
@@ -0,0 +1,22 @@
+/**
+ * Category: ARRAYS
+ * Tags: easy arrays
+ *
+ * Given an integer array nums and an integer val, remove all
+ * occurrences of val in nums in-place. The order of the elements
+ * may be changed. Then return the number of elements in nums which
+ * are not equal to val.
+ */
+import { assert } from "console";
+function removeElement(nums, val) {
+    for (let i = 0; i < nums.length;) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+        }
+        else {
+            i++;
+        }
+    }
+    return nums.length;
+}
+assert(removeElement([1, 2, 3, 4, 5, 3, 4], 3) === 5);
