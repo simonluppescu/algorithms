a29da13d496c72d92d57e2db7728f32863e21b77
Simon <simon.luppescu@gmail.com>
2024-04-11 00:06:21 +0900

    Added better Tower of Hanoi problem

---
+++ js_output/towerOfHanoiDynamic.js
@@ -0,0 +1,41 @@
+/**
+ * Category: DYNAMIC_PROGRAMMING
+ * Tags: recursion stacks
+ *
+ * Solve the Tower of Hanoi game
+ * This solution uses dynamic programming to break the problem into smaller
+ * problems. This one should be more efficient than the other one.. although there
+ * is no memoization happening here..
+ *
+ */
+import Stack from "./utils/stack.js";
+class Tower extends Stack {
+    toString() {
+        return `|${this.stack.join(" ")}|`;
+    }
+}
+function solve(height, origin, buffer, dest) {
+    if (height === 2) {
+        move(origin, buffer);
+        move(origin, dest);
+        move(buffer, dest);
+        return;
+    }
+    console.log(origin.toString(), buffer.toString(), dest.toString());
+    solve(height - 1, origin, dest, buffer);
+    move(origin, dest);
+    solve(height - 1, buffer, origin, dest);
+    return dest;
+}
+function move(origin, dest) {
+    dest.push(origin.pop());
+}
+function run(height) {
+    const origin = new Tower();
+    for (let i = height; i > 0; i--) {
+        origin.push(i);
+    }
+    const result = solve(height, origin, new Tower(), new Tower());
+    console.log("Destination is now", result.toString());
+}
+run(5);
